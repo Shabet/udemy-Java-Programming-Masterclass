256f34177c18575b2e529836f916fcb5d1d64bb6
JunYong Park <pakjunyoung@hanmail.net>
2024-03-10 16:27:01 +0900

    ex02 잘못 적용된 소스 및 코드 정리

--- src/ch12/ex02_Generics_Part2/Main.java
+++ src/ch12/ex02_Generics_Part2/Main.java
@@ -5,36 +5,61 @@ interface Player {
 }
 record BaseballPlayer(String name, String position) implements Player {}
 record FootballPlayer(String name, String position) implements Player {}
-record VollyballPlayer(String name, String position) implements Player {}
 
 public class Main {
     public static void main(String[] args) {
+        /*
+         * Solution1: duplicate code
+         *            BaseballTeam ---<> BaseballPlayer
+         *            FootballTeam ---<> FootballPlayer
+         * Solution2: Use Player interface or abstract class to support different types of players
+         *            SportsTeam ---<> Player
+         *                               |
+         *                               +--- BaseballPlayer
+         *                               +--- FootballPlayer
+         * Solution3: Use generics into Solution2
+         *            Team ---<> Player
+         *                         |
+         *                         +--- BaseballPlayer
+         *                         +--- FootballPlayer
+         */
         BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
         BaseballTeam astros1 = new BaseballTeam("Houston Astros");
         scoreResult(phillies1, 3, astros1, 5);
 
+        // use Solution2
         SportsTeam phillies2 = new SportsTeam("Philadelphia Phillies");
         SportsTeam astros2 = new SportsTeam("Houston Astros");
         scoreResult(phillies2, 3, astros2, 5);
 
-        SportsTeam phillies = new SportsTeam("Philadelphia Phillies");
-        SportsTeam astros = new SportsTeam("Houston Astros");
-        scoreResult(phillies, 3, astros, 5);
-
         var harper = new BaseballPlayer("B Harper", "Right Fielder");
         var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
-        phillies.addTeamMember(harper);
-        phillies.addTeamMember(marsh);
-        phillies.listTeamMembers();
+        phillies2.addTeamMember(harper);
+        phillies2.addTeamMember(marsh);
+        phillies2.listTeamMembers();
 
+        // Solution2 problem
         SportsTeam afc = new SportsTeam("Adelaide Crows");
         var tex = new FootballPlayer("Tex Walker", "Centre half forward");
         afc.addTeamMember(tex);
-//        afc.listTeamMembers();
-
         var guthrie = new BaseballPlayer("D Guthrie", "Center Fielder");
         afc.addTeamMember(guthrie);
-        afc.listTeamMembers();
+        afc.listTeamMembers(); // 의도하지 않게 축구, 야구 선수가 모두 출력됨.
+
+        // Use Solution3
+        Team<BaseballPlayer> phillies = new Team<>("Philadelphia Phillies");
+        Team<BaseballPlayer> astros = new Team<>("Houston Astros");
+        scoreResult(phillies, 3, astros, 5);
+
+        Team<BaseballPlayer> afc2 = new Team<>("Adelaide Crows");
+//        afc2.addTeamMember(tex); //error
+        afc2.addTeamMember(guthrie);
+
+        Team<FootballPlayer> afc3 = new Team<>("Adelaide Crows");
+        afc3.addTeamMember(tex);
+        var rory = new FootballPlayer("Rory Laird", "Midfield");
+        afc3.addTeamMember(rory);
+        afc3.listTeamMembers();
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
@@ -46,6 +71,13 @@ public class Main {
 
     public static void scoreResult(SportsTeam team1, int t1_score,
                                    SportsTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(Team team1, int t1_score,
+                                   Team team2, int t2_score) {
         String message = team1.setScore(t1_score, t2_score);
         team2.setScore(t2_score, t1_score);
         System.out.printf("%s %s %s %n", team1, message, team2);
--- src/ch12/ex02_Generics_Part2/Team.java
+++ src/ch12/ex02_Generics_Part2/Team.java
@@ -2,28 +2,17 @@ package ch12.ex02_Generics_Part2;
 
 import java.util.ArrayList;
 import java.util.List;
-record Affiliation(String name, String type, String countryCode) {
-    @Override
-    public String toString() {
-        return name + " (" + type + " in " + countryCode + ")";
-    }
-}
-public class Team<T extends Player, S> {
+public class Team<T> {
     private String teamName;
     private List<T> teamMembers = new ArrayList<>();
     private int totalWins  = 0;
     private int totalLosses = 0;
     private int totalTies = 0;
-    private S affiliation;
 
     public Team(String teamName) {
         this.teamName = teamName;
     }
 
-    public Team(String teamName, S affiliation) {
-        this.teamName = teamName;
-        this.affiliation = affiliation;
-    }
 
     public void addTeamMember(T t) {
         if (!teamMembers.contains(t)) {
@@ -33,11 +22,7 @@ public class Team<T extends Player, S> {
 
     public void listTeamMembers() {
         System.out.print(teamName + " Roster:");
-        System.out.println((affiliation == null ? "" : " AFFILIATION: " + affiliation));
-//        System.out.println(teamMembers);
-        for (T t : teamMembers) {
-            System.out.println(t.name());
-        }
+        System.out.println(teamMembers);
     }
 
     public int ranking() {
