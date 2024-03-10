936380cc25d01a483f2e056b9cc9b5fa3fc13551
JunYong Park <pakjunyoung@hanmail.net>
2024-03-10 15:45:05 +0900

    사용되지 않은 코드 삭제

--- src/ch12/ex01_Generics_Part1/Main.java
+++ src/ch12/ex01_Generics_Part1/Main.java
@@ -1,9 +1,6 @@
 package ch12.ex01_Generics_Part1;
 
-interface Player {
-    String name();
-}
-record BaseballPlayer(String name, String position) implements Player {}
+record BaseballPlayer(String name, String position) {}
 
 public class Main {
     public static void main(String[] args) {
